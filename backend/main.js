ee9f27292c1e91f2e1ec5907bff95d0c8b9d2c5e
Austin McLain <93621884+austinmclain@users.noreply.github.com>
2023-12-08 22:28:24 -0600

    Create routes for notes in backend

--- backend/main.js
+++ backend/main.js
@@ -7,10 +7,13 @@ const port = 8000
 dotenv.config()
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Connects to database
 const db = process.env.CONNECTION_STRING
@@ -27,6 +30,18 @@ const User = mongoose.model('User', {
   password: { type: String }
 })
 
+// Note schema
+const noteSchema = new mongoose.Schema({
+  author: { type: String },
+  title: { type: String },
+  content: { type: String }
+  },
+  { timestamps: true }
+)
+
+// Note model
+const Note = mongoose.model('Note', noteSchema)
+
 // Login route
 app.get('/login', async (req, res) => {
   const user = await User.findOne({ username: req.query.username }).exec()
@@ -35,6 +50,22 @@ app.get('/login', async (req, res) => {
   } else {
     res.json(false)
   }
+})
+
+// New note route
+app.post('/new_note', async (req, res) => {
+  await Note.create({
+    author: req.body.author,
+    title: req.body.title,
+    content: req.body.content
+  })
+  res.json(true)
+})
+
+// Get notes route
+app.get('/notes', async (req, res) => {
+  const notes = await Note.find({ "author": req.query.author }).exec()
+  res.json(notes)
 })
 
 app.listen(port, () => {
